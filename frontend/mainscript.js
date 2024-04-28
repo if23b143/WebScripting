09a5866f842723bbfd741903366311e488400465
test <test.com>
2024-04-28 17:05:46 +0200

    Frontend: Einige Änderungen gemacht - Liste für Meetings erstellt / Neuer Appointment Button / Angefangen mit Details anzeigen

--- frontend/mainscript.js
+++ frontend/mainscript.js
@@ -4,8 +4,43 @@ $(document).ready(function () {
     $("#btn_Search").click(function (e) {
        loaddata($("#seachfield").val());
     });
+    $("#new_appointment").click(function() {
+        new_appointment();
+    });
 
+    $(".list-group .list-group-item small").hide();
+
+    
+    
+    // Event-Delegation für das Klicken auf Elemente mit der Klasse "list-group-item"
+    $(".list-group").on("click", ".list-group-item", function(e) {
+        console.log("hallo");
+
+        e.stopPropagation();
+
+        // Zeige das <small>-Element innerhalb des angeklickten list-group-items an
+        //VIELLEICHT COOL DAS ANGEZEIGT WIRD--WENN ER ABGESTIMMT HAT
+        $(this).find("small").show();
+
+        //$(".list-group .list-group-item small").show();
+        new_appointment();
+
+        $("#overlay").show();
+    });
+
+    // Close the overlay when clicking outside the white box
+    $(document).on("click", function(e) {
+        if (!$(e.target).closest('.white-box').length) {
+            $("#overlay").hide();
+        }
+    });
 });
+
+function new_appointment() {
+    //alert("Hallo");
+    
+}
+
 
 function loaddata(searchterm) {
 
