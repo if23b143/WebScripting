b7031d89b6b1e3e58fa19684f0c7e0c5f750f801
Karlheinz <Karlheinz.Lunatschek@drei.at>
2024-05-01 18:59:24 +0200

    fix

--- backend/models/appointment.php
+++ backend/models/appointment.php
@@ -5,13 +5,20 @@ class appointment {
     public $ort;
     public $datum;
     public $ablaufdatum;
+    public $Auswahl1;
+    public $Auswahl2;
+    public $Auswahl3;
     
 
-    function __construct($id, $titel, $ort, $datum, $ablaufdatum,) {
+    function __construct($id, $titel, $ort, $datum, $ablaufdatum, $Auswahl1, $Auswahl2, $Auswahl3) {
         $this->id = $id;
         $this->titel = $titel;
         $this->ort = $ort;
         $this->datum = $datum;
         $this->ablaufdatum = $ablaufdatum;
+        $this->Auswahl1 = $Auswahl1
+        $this->Auswahl2 = $Auswahl2;
+        $this->Auswahl3 = $Auswahl3;
+
       }
 }
